1d34489d45759b9a03fcc51a456952bc8097b240
CibinBK <zephanphilip03@gmail.com>
2024-06-26 21:48:14 +0530

    bugs fixed

--- server/server.js
+++ server/server.js
@@ -22,10 +22,10 @@ app.use(express.json())
 app.use('/api/notes',noteRoutes);
 
 // Serve static files
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function(req, res) { 
-    res.sendFile(path.join(__dirname, './build', 'index.html')); 
+    res.sendFile(path.join(__dirname, 'build', 'index.html')); 
 });
 
 
