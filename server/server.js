1a7bf9110e95e4f8a9a22f94d9544530eef55525
CibinBK <zephanphilip03@gmail.com>
2024-06-26 22:09:06 +0530

    bugs fixed

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const path = require('path');
 
 const noteRoutes = require('./routes/noteRouter');
 
